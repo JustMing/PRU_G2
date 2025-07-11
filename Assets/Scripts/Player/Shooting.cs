f8d1ce671772bd919c35db9bb447997459242dd3
Minh Nguyen <nqm061204@gmail.com>
2025-07-11 21:44:59 +0700

    f:v

--- Assets/Scripts/Player/Shooting.cs
+++ Assets/Scripts/Player/Shooting.cs
@@ -5,11 +5,6 @@ public class Shooting : MonoBehaviour
     [SerializeField]private Transform firePoint;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
